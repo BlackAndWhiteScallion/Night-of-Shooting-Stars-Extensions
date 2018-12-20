a6f71a63399378f102d6e50e3436faeeba7c120d
BWS <tanguo@hotmail.com>
2018-12-20 00:00:03 -0500

    Create catalog.js

---
+++ catalog.js
@@ -0,0 +1,14 @@
+extension["游戏更新！"]={
+	intro:"<div style='font-size:12px;'>这里是流星夜的更新！<br> 更新完毕后，把extensions文件夹里的 ",
+	author:"官方",
+	netdisk:"https://pan.baidu.com/s/1T75jFtfbOP1QasNP-sSs7w",
+	forum:"https://mp.weixin.qq.com/s/6vgVrSQFQsqHcyGJ1xIDow",
+	version:"1.0",
+	files:["cxyCaoCao.jpg","cxyCaoHong.jpg","cxyCaoRen.jpg","cxyChengYu.jpg","cxyChenLin.jpg",
+		"cxyChunYuQiong.jpg","cxyGaoLan.jpg","cxyGuanYu.jpg","cxyGuoFeng.jpg","cxyGuoJia.jpg",
+		"cxyHanShi.jpg","cxyJuShou.jpg","cxyLiuBei.jpg","cxyLiuYe.jpg","cxyShenPei.jpg",
+		"cxyTianFeng.jpg","cxyTunLiang.png","cxyXuHuang.jpg","cxyXunChen.jpg","cxyXunYou.jpg",
+		"cxyXunYu.jpg","cxyXuYou.jpg","cxyYanWen.jpg","cxyYuanJun.png","cxyYuanShao.jpg",
+		"cxyYuJin.jpg","cxyZhangHe.jpg","cxyZhangLiao.jpg","extension.js","style.css"],
+	size:"2.66MB"
+};
