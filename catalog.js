afcc0f595fb38916d78a1fa64ea3215b88a3baee
BWS <tanguo@hotmail.com>
2018-12-20 12:33:52 -0500

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,14 +1,8 @@
 extension["游戏更新！"]={
-	intro:"<div style='font-size:12px;'>这里是流星夜的更新！<br> 更新完毕后，把extensions文件夹里的 ",
+	intro:"<div style='font-size:12px;'>这里是流星夜的更新！<br> 下载后记得解压到源文件夹里去！",
 	author:"官方",
-	netdisk:"https://pan.baidu.com/s/1T75jFtfbOP1QasNP-sSs7w",
-	forum:"https://mp.weixin.qq.com/s/6vgVrSQFQsqHcyGJ1xIDow",
+	netdisk:"https://github.com/BlackAndWhiteScallion/Night-of-Shooting-Stars-Extensions/blob/master/_walfas__custom_hands_v2_by_ceej39-d8iwpbj.zip?raw=true",
 	version:"1.0",
-	files:["cxyCaoCao.jpg","cxyCaoHong.jpg","cxyCaoRen.jpg","cxyChengYu.jpg","cxyChenLin.jpg",
-		"cxyChunYuQiong.jpg","cxyGaoLan.jpg","cxyGuanYu.jpg","cxyGuoFeng.jpg","cxyGuoJia.jpg",
-		"cxyHanShi.jpg","cxyJuShou.jpg","cxyLiuBei.jpg","cxyLiuYe.jpg","cxyShenPei.jpg",
-		"cxyTianFeng.jpg","cxyTunLiang.png","cxyXuHuang.jpg","cxyXunChen.jpg","cxyXunYou.jpg",
-		"cxyXunYu.jpg","cxyXuYou.jpg","cxyYanWen.jpg","cxyYuanJun.png","cxyYuanShao.jpg",
-		"cxyYuJin.jpg","cxyZhangHe.jpg","cxyZhangLiao.jpg","extension.js","style.css"],
+	files:["extension.js"],
 	size:"2.66MB"
 };
