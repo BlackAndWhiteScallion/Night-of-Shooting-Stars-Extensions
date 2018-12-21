48810f878e9427b91b5f9fd2db753ce7f082e04d
BWS <tanguo@hotmail.com>
2018-12-20 21:00:55 -0500

    i legit do not understand git
    why does this not update?

--- catalog.js
+++ catalog.js
@@ -1,7 +1,7 @@
 extension["游戏更新！"]={
 	intro:"<div style='font-size:12px;'>这里是流星夜的更新！<br> 下载后记得解压到源文件夹里去！",
 	author:"官方",
-	netdisk:"https://github.com/BlackAndWhiteScallion/Night-of-Shooting-Stars-Updates/archive/master.zip",
+	netdisk:"https://github.com/BlackAndWhiteScallion/Night-of-Shooting-Stars-Extensions/archive/master.zip",
 	version:"1.0",
 	files:["extension.js"],
 	size:"?MB"
