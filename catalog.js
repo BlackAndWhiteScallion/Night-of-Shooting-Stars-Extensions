93c2c7ad0546497dc0196eb48663c4108642efd4
BWS <tanguo@hotmail.com>
2018-12-22 12:11:24 -0500

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,7 +1,15 @@
-extension["游戏更新！"]={
+extension["游戏更新！（国外镜像）"]={
 	intro:"<div style='font-size:12px;'>这里是流星夜的更新！<br> 下载后记得解压到源文件夹里去！",
 	author:"官方",
 	netdisk:"https://github.com/BlackAndWhiteScallion/Night-of-Shooting-Stars-Extensions/archive/master.zip",
+	version:"1.0",
+	files:["extension.js"],
+	size:"?MB"
+};
+extension["游戏更新！（国内镜像）"]={
+	intro:"<div style='font-size:12px;'>这里是流星夜的更新！<br> 下载后记得解压到源文件夹里去！",
+	author:"官方",
+	netdisk:"https://coding.net/u/BWS/p/NOSS-Extensions/git/archive/master",
 	version:"1.0",
 	files:["extension.js"],
 	size:"?MB"
